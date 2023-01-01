d47169f1b9b0c806e785a3734573c36d134f4c97
“NicolasAT08” <nicolasaarangurent@gmail.com>
2023-01-01 11:47:25 -0500

    Setup easy deployment using docker.

--- manage_dev.py
+++ manage_dev.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_auth_example.settings_prod') # Se ajusta el settings_prod en este archivo para producción
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_auth_example.settings_dev') # el settings.py es solo para desarrollo y trabajo local
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
